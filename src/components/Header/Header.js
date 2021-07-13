e81615f9e3a650b6fd2871bd16c38bb845c49d30
thals1214 <thaxls@ewhain.net>
2021-07-13 23:40:35 +0900

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,17 @@
-import styled from "styled-components";
-import { useDispatch } from 'react-redux';
-import { useHistory } from "react-router-dom";
-import Logo from "../../assets/logo.svg"
-import Search from "../../assets/search.svg"
-import Vector from "../../assets/Vector.svg"
+import styled from 'styled-components'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import Logo from '../../assets/logo.svg'
+import Search from '../../assets/search.svg'
+import Vector from '../../assets/Vector.svg'
 
 const MyIcon = styled.img`
-margin-top: 8px;
+  margin-top: 8px;
   @media (min-width: 375px) and (max-width: 1440px) {
     //between
     width: 127px;
     margin-right: 20px;
-    margin-left: 10px;;
-  
+    margin-left: 10px;
   }
 
   @media (min-width: 1440px) {
@@ -20,24 +19,23 @@ margin-top: 8px;
     width: 127px;
     margin-right: 168px;
     margin-left: 76px;
-
   }
   @media (max-width: 375px) {
     //iphone
     width: 84px;
     display: center;
   }
-`;
+`
 
 MyIcon.defaultProps = {
   src: Logo,
-};
+}
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
-  height:66px;
+  height: 66px;
   justify-content: space-between;
 
   margin-top: 3px;
@@ -45,53 +43,49 @@ const Wrapper = styled.div`
 
 const Line = styled.div`
   width: 0px;
-  margin-top:19px;
-  border: 1px solid #A3A3A3;
+  margin-top: 19px;
+  border: 1px solid #a3a3a3;
   @media (min-width: 375px) and (max-width: 1440px) {
     //between
     margin-left: 22px;
     margin-right: 22px;
-    height:26px;
-    
+    height: 26px;
   }
 
   @media (min-width: 1440px) {
     //desktop
     margin-left: 62px;
     margin-right: 62px;
-    height:26px;
-
+    height: 26px;
   }
   @media (max-width: 375px) {
     //iphone
     display: none;
   }
-  
 `
 const Input = styled.input`
-    color:#000000;
-    background-color: #F7F3F3;
-    line-height : 20px;
-    font-size: 16px;
-    overflow-wrap:"auto";
-    height: 30px;
-    border: 1px solid #ffffff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    outline:none;
-    
-    margin-top:11px;
-    border-radius: 10px;
-    @media (min-width: 375px) and (max-width: 1440px) {
+  color: #000000;
+  background-color: #f7f3f3;
+  line-height: 20px;
+  font-size: 16px;
+  overflow-wrap: 'auto';
+  height: 30px;
+  border: 1px solid #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  outline: none;
+
+  margin-top: 11px;
+  border-radius: 10px;
+  @media (min-width: 375px) and (max-width: 1440px) {
     //between
-    width:150px;
+    width: 150px;
     margin-left: 45px;
   }
 
   @media (min-width: 1440px) {
     //desktop
-    width:368px;
+    width: 368px;
     margin-left: 85px;
-
   }
   @media (max-width: 375px) {
     //iphone
@@ -99,36 +93,33 @@ const Input = styled.input`
   }
 `
 const Button = styled.button`
- margin-top:${(props) => props.margin || 19}px;
-  height:  31px ;
-  border: 1px solid rgba( 255, 255, 255, 0 );
-  color:black;
+  margin-top: ${props => props.margin || 19}px;
+  height: 31px;
+  border: 1px solid rgba(255, 255, 255, 0);
+  color: black;
   background: white;
-  
+
   font-family: 'NotoSans';
-  outline:none;
+  outline: none;
   @media (min-width: 375px) and (max-width: 1440px) {
     //between
-    width:70px;
-    font-size:14px;
+    width: 70px;
+    font-size: 14px;
   }
 
   @media (min-width: 1440px) {
     //desktop
-    width:100px;
-    font-size:  17px;
-
+    width: 100px;
+    font-size: 17px;
   }
   @media (max-width: 375px) {
     //iphone
     display: none;
-
   }
 `
 const SearchButton = styled.button`
- 
-  outline:none;
-  border: 1px solid rgba( 255, 255, 255, 0 );
+  outline: none;
+  border: 1px solid rgba(255, 255, 255, 0);
   border-radius: 10px;
   width: 40px;
   height: 37px;
@@ -137,7 +128,7 @@ const SearchButton = styled.button`
     //between
     margin-right: 10px;
     margin-left: 10px;
-    background-color: #F7F3F3;
+    background-color: #f7f3f3;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   }
 
@@ -145,23 +136,20 @@ const SearchButton = styled.button`
     //desktop
     margin-right: 116px;
     margin-left: 20px;
-    background-color: #F7F3F3;
+    background-color: #f7f3f3;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-
   }
   @media (max-width: 375px) {
     //iphone
     background-color: white;
     box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.25);
-    
   }
-  
 `
 
 const ToggleButton = styled.button`
   background-color: white;
-  outline:none;
-  border: 1px solid rgba( 255, 255, 255, 0 );
+  outline: none;
+  border: 1px solid rgba(255, 255, 255, 0);
 
   width: 40px;
   height: 37px;
@@ -174,28 +162,29 @@ const ToggleButton = styled.button`
   @media (min-width: 1440px) {
     //desktop
     display: none;
-
   }
-
 `
 
 function Header() {
-  const history = useHistory();
+  const history = useHistory()
   return (
     <Wrapper>
-      <ToggleButton><img style={{ width: "15px", height: "15px" }} src={Vector} /></ToggleButton>
+      <ToggleButton>
+        <img style={{ width: '15px', height: '15px' }} src={Vector} />
+      </ToggleButton>
       <MyIcon></MyIcon>
-      <Button onClick={() => history.push(`/`)}>  날씨 </Button>
+      <Button onClick={() => history.push(`/`)}> 날씨 </Button>
       <Line />
       <Button onClick={() => history.push(`/dust`)}> 미세먼지 </Button>
       <Line />
-      <Button onClick={() => history.push(`/covid`)}>  코로나 </Button>
+      <Button onClick={() => history.push(`/covid`)}> 코로나 </Button>
       <Input></Input>
-      <SearchButton> <img style={{ width: "30px", height: "30px" }} src={Search} /></SearchButton>
+      <SearchButton>
+        {' '}
+        <img style={{ width: '30px', height: '30px' }} src={Search} />
+      </SearchButton>
     </Wrapper>
-
-  );
+  )
 }
 
-
-export default Header;
+export default Header
