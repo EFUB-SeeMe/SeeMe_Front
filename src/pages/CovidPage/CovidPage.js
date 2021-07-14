c023068d1fd86a163635a784eb28eb92829a311d
thals1214 <thaxls@ewhain.net>
2021-07-14 18:25:26 +0900

    Current Location specifics ( in between (0) mobile (X))

--- src/pages/CovidPage/CovidPage.js
+++ src/pages/CovidPage/CovidPage.js
@@ -59,12 +59,158 @@ const Wrap2 = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+
+  @media (max-width: 375px) {
+    //iphone
+    display: none;
+  }
+`
+const Loc_Icon = styled.button`
+  left: 18.12%;
+  right: 80.14%;
+  top: 10.84%;
+  bottom: 86.91%;
+  outline: none;
+  border: none;
+  margin-left: 37px;
+  margin-top: 21px;
+  background: white;
+`
+const Text1 = styled.a`
+  width: 195px;
+  height: 32px;
+  left: 296.01px;
+  top: 105px;
+  margin-top: 15px;
+  font-family: 'NotoSans';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 32px;
+
+  @media (min-width: 375px) and (max-width: 1440px) {
+    //between
+    font-size: 20px;
+    line-height: 32px;
+  }
+`
+const Text2 = styled.div`
+  width: 200px;
+  height: 34px;
+  left: 376px;
+  top: 161px;
+  margin-left: 152px;
+
+  font-family: 'NotoSans';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  line-height: 34px;
+  color: #000000;
+
+  @media (max-width: 375px) {
+    //iphone
+    display: none;
+  }
+  @media (min-width: 375px) and (max-width: 1440px) {
+    //between
+    font-size: 22px;
+    margin-left: 130px;
+  }
+`
+const Wrap2a = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+const Text3 = styled.div`
+  width: 185px;
+  height: 95px;
+  left: 375px;
+  top: 196px;
+  margin-left: 152px;
+
+  font-family: 'NotoSans';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 70px;
+  line-height: 95px;
+  color: #000000;
+
+  @media (max-width: 375px) {
+    //iphone
+    display: none;
+  }
+  @media (min-width: 375px) and (max-width: 1440px) {
+    //between
+    font-size: 60px;
+    margin-left: 140px;
+  }
 `
 const Wrap3 = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`
+const Line = styled.div`
+  width: 0px;
+  height: 183.5px;
+  left: 718px;
+  top: 150.5px;
+  border: 1.5px solid #d9d9d9;
+
+  margin-left: 158px;
+  margin-top: 15px;
+  @media (max-width: 375px) {
+    //iphone
+    width: 0px;
+    height: 106px;
+    left: 187px;
+    top: 120px;
+    border: 1.5px solid #d9d9d9;
+  }
+  @media (min-width: 375px) and (max-width: 1440px) {
+    //between
+    margin-left: 95px;
+  }
+`
+const Ascent = styled.image`
+  width: 99px;
+  height: 37px;
+  left: 410px;
+  top: 302px;
+  margin-top: 11px;
+  margin-left: 150px;
+
+  @media (max-width: 375px) {
+    //iphone
+  }
+  @media (min-width: 375px) and (max-width: 1440px) {
+    //between
+    margin-left: 100px;
+  }
+`
+const Descent = styled.image`
+  width: 99px;
+  height: 37px;
+  left: 925px;
+  top: 302px;
+  margin-top: 11px;
+  margin-left: 150px;
+
+  @media (max-width: 375px) {
+    //iphone
+    width: 0px;
+    height: 106px;
+    left: 187px;
+    top: 120px;
+    border: 1.5px solid #d9d9d9;
+  }
+  @media (min-width: 375px) and (max-width: 1440px) {
+    //between
+    margin-left: 100px;
+  }
 `
 const Box2 = styled.div`
   background: white;
@@ -128,89 +274,28 @@ function CovidPage() {
           <Box1>
             <Wrap1>
               <Wrap2>
-                <img
-                  style={{ marginTop: '21px', marginLeft: '37px' }}
-                  src={cur_location}
-                />
-                <a
-                  style={{
-                    marginTop: '15px',
-                    marginLeft: '10px',
-                    fontSize: '22px',
-                    fontFamily: 'NotoSans',
-                  }}
-                >
-                  현재 위치(00구 00동)
-                </a>
+                <Loc_Icon>
+                  <img src={cur_location} />
+                </Loc_Icon>
+                <Text1>현재 위치(00구 00동)</Text1>
               </Wrap2>
-              <Wrap2>
+              <Wrap2a>
                 <Wrap3>
-                  <a
-                    style={{
-                      marginTop: '24px',
-                      marginLeft: '160px',
-                      fontSize: '25px',
-                      fontFamily: 'NotoSans',
-                    }}
-                  >
-                    오늘의 확진자 수
-                  </a>
-                  <a
-                    style={{
-                      marginLeft: '160px',
-                      fontSize: '70px',
-                      fontFamily: 'NotoSans',
-                    }}
-                  >
-                    567명
-                  </a>
-                  <img
-                    style={{
-                      marginTop: '10px',
-                      marginLeft: '182px',
-                      height: '37px',
-                      width: '99px',
-                    }}
-                    src={ascent_dif}
-                  ></img>
+                  <Text2>오늘의 확진자 수</Text2>
+                  <Text3>567명</Text3>
+                  <Ascent>
+                    <img src={ascent_dif}></img>
+                  </Ascent>
                 </Wrap3>
-                <img
-                  style={{
-                    marginLeft: '160px',
-                  }}
-                  src={dividing_line}
-                ></img>
+                <Line></Line>
                 <Wrap3>
-                  <a
-                    style={{
-                      marginTop: '24px',
-                      marginLeft: '152px',
-                      fontSize: '25px',
-                      fontFamily: 'NotoSans',
-                    }}
-                  >
-                    우리지역 확진자 수
-                  </a>
-                  <a
-                    style={{
-                      marginLeft: '152px',
-                      fontSize: '70px',
-                      fontFamily: 'NotoSans',
-                    }}
-                  >
-                    334명
-                  </a>
-                  <img
-                    style={{
-                      marginTop: '10px',
-                      marginLeft: '186px',
-                      height: '37px',
-                      width: '99px',
-                    }}
-                    src={descent_dif}
-                  ></img>
+                  <Text2 style={{}}>우리지역 확진자 수</Text2>
+                  <Text3>334명</Text3>
+                  <Descent>
+                    <img src={descent_dif}></img>
+                  </Descent>
                 </Wrap3>
-              </Wrap2>
+              </Wrap2a>
             </Wrap1>
           </Box1>
           <Box2></Box2>
