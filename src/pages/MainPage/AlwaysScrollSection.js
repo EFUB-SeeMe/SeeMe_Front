7aef8975133bed231faacd32fc6102c1730542fd
정아연 <ribbon0529@naver.com>
2021-07-16 00:50:30 +0900

    add Main

---
+++ src/pages/MainPage/AlwaysScrollSection.js
@@ -0,0 +1,27 @@
+import React, { memo } from "react";
+import styled from "styled-components";
+
+export const AlwaysScrollSection = memo(props => {
+  const { children } = props;
+  return <StyledAlwaysScrollSection>{children}</StyledAlwaysScrollSection>;
+});
+
+const StyledAlwaysScrollSection = styled.div`
+  overflow: scroll;
+  display: flex;
+  
+  &::-webkit-scrollbar {
+    /* 세로 스크롤 넓이 */
+    width: 8px;
+
+    /* 가로 스크롤 높이 */
+    height: 8px;
+
+    border-radius: 6px;
+    background: rgba(255, 255, 255, 0.4);
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.3);
+    border-radius: 6px;
+  }
+`;
