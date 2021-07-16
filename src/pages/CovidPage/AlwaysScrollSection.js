9e73023a4d775e3459150cb70d0ef0fede3ae3f0
thals1214 <thaxls@ewhain.net>
2021-07-16 20:00:46 +0900

    Update AlwaysScrollSection.js

--- src/pages/CovidPage/AlwaysScrollSection.js
+++ src/pages/CovidPage/AlwaysScrollSection.js
@@ -12,12 +12,11 @@ const StyledAlwaysScrollSection = styled.div`
 
   &::-webkit-scrollbar {
     /* 세로 스크롤 넓이 */
-    width: 8px;
+    width: 1px;
 
     /* 가로 스크롤 높이 */
-    height: 8px;
-
-    border-radius: 6px;
+    height: 2px;
+    border-radius: 3px;
     background: rgba(255, 255, 255, 0.4);
   }
   &::-webkit-scrollbar-thumb {
