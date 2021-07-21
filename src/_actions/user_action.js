9d720b16ce1957380234b602b08409471e4ca8ba
Chang Ye Jun <78217921+Asolatte@users.noreply.github.com>
2021-07-22 02:55:30 +0900

    통합대기환경지수 레이아웃

--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -1,42 +1,44 @@
 import axios from 'axios'
-import {COVID_MAIN,COVID_NATIONAL,COVID_REGIONAL,LAT_TO_ADD} from './type'
+import { COVID_MAIN, COVID_NATIONAL, COVID_REGIONAL, LAT_TO_ADD } from './type'
 
-import { USER_SERVER } from "../config";
+import { USER_SERVER } from '../config'
 
-
-
-export const covidMain =(lat, lon) =>{
-    const request=axios.get(`${USER_SERVER}/covid/main?lat=${lat}&lon=${lon}`)
+export const covidMain = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/covid/main?lat=${lat}&lon=${lon}`)
     .then(response => response.data)
-    return{
-        type:COVID_MAIN,
-        payload:request
-    }
+  return {
+    type: COVID_MAIN,
+    payload: request,
+  }
 }
 
-export const covidNational =() =>{
-    const request=axios.get(`${USER_SERVER}/covid/national`)
+export const covidNational = () => {
+  const request = axios
+    .get(`${USER_SERVER}/covid/national`)
     .then(response => response.data)
-    return{
-        type:COVID_NATIONAL,
-        payload:request
-    }
+  return {
+    type: COVID_NATIONAL,
+    payload: request,
+  }
 }
 
-export const covidRegional =(lat, lon) =>{
-    const request=axios.get(`${USER_SERVER}/covid/regional?lat=${lat}&lon=${lon}`)
+export const covidRegional = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/covid/regional?lat=${lat}&lon=${lon}`)
     .then(response => response.data)
-    return{
-        type:COVID_REGIONAL,
-        payload:request
-    }
+  return {
+    type: COVID_REGIONAL,
+    payload: request,
+  }
 }
 
-export const latToAdd =(lat, lon) =>{
-    const request=axios.get(`${USER_SERVER}/location/latLonToUmd?lat=${lat}&lon=${lon}`)
+export const latToAdd = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/location/latLonToUmd?lat=${lat}&lon=${lon}`)
     .then(response => response.data)
-    return{
-        type:LAT_TO_ADD,
-        payload:request
-    }
+  return {
+    type: LAT_TO_ADD,
+    payload: request,
+  }
 }
--- src/pages/DustPage/DustPage.js
+++ src/pages/DustPage/DustPage.js
@@ -10,6 +10,7 @@ import dust from '../../assets/Group 336.svg'
 import location from '../../assets/Dust_location.svg'
 import blue from '../../assets/Dust_blue.svg'
 import standard from '../../assets/Dust_standard.svg'
+import face from '../../assets/face_bad.svg'
 
 import Dustgraph_day from './Dustgraph_day'
 import Dustgraph_today from './Dustgraph_today'
@@ -188,7 +189,7 @@ const Box4 = styled.div`
   @media (min-width: 430px) and (max-width: 1440px) {
     //between
     width: 90%;
-    height: 350px;
+    height: 380px;
   }
   @media (min-width: 1440px) {
     //desktop
@@ -274,7 +275,53 @@ const Text2 = styled.div`
     font-size: ${props => props.size - 10 || 18}px;
   }
 `
+const FaceImage = styled.img`
+  width: 350px;
+  height: 430px;
+  @media (min-width: 430px) and (max-width: 1440px) {
+    //between
+    width: 230px;
+    height: 120px;
+  }
+  @media (max-width: 430px) {
+    //iphone
+    margin-top: 10px;
+    width: 60%;
+    height: 60%;
+  }
+`
+const Box4_sub1 = styled.div`
+  // 통합대기환경지수 박스
+  display: flex;
+  flex-direction: column;
+  align-items: center; // 가로 정렬
+  //align-items: left;
 
+  height: 100%;
+`
+const Text3 = styled.button`
+  align-items: center;
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 22px;
+  font-weight: regular;
+  background: white;
+  border: none;
+  outline: none;
+  @media (min-width: 430px) and (max-width: 1440px) {
+    //between
+    font-size: 20px;
+    margin-left: 40px;
+    margin-right: 20px;
+  }
+  @media (max-width: 430px) {
+    //iphone
+    font-size: 15px;
+    width: 80px;
+    height: 25px;
+  }
+`
 
 function DustPage() {
   return (
@@ -300,9 +347,7 @@ function DustPage() {
             </MainBox>
             <Dustinfo />
           </Box1>
-          <Box2>
-            
-          </Box2>
+          <Box2></Box2>
 
           <Box2>
             <Box2_sub1>
@@ -357,6 +402,28 @@ function DustPage() {
             <Dustinfo2 />
           </Box3>
           <Box4>
+            <Text> 통합대기환경지수 </Text>
+            <Row>
+              <FaceImage src={face} />
+            </Row>
+            <Box4_sub1>
+              <Row>
+                <Text3> 아황산가스 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+              <Row>
+                <Text3> 일산화탄소 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+              <Row>
+                <Text3> 오존 </Text3>
+                <Text3>&nbsp;&ensp;&ensp;&emsp; 000ppm </Text3>
+              </Row>
+              <Row>
+                <Text3> 이산화질소 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+            </Box4_sub1>
           </Box4>
         </Wrapper2>
 
@@ -378,20 +445,21 @@ function DustPage() {
                 </a>
               </Row>
               <Row>
-                <Text2 size="60" color="#42A0F0">  23 &emsp; </Text2>
-                <Text2 size="60" color="#42A0F0">  15 </Text2>
+                <Text2 size="60" color="#42A0F0">
+                  {' '}
+                  23 &emsp;{' '}
+                </Text2>
+                <Text2 size="60" color="#42A0F0">
+                  {' '}
+                  15{' '}
+                </Text2>
               </Row>
               <Row>
-                <img
-                  style={{ marginTop: '5px'}}
-                  src={standard}
-                />
+                <img style={{ marginTop: '5px' }} src={standard} />
               </Row>
             </div>
           </Box1_mobile>
-          <Box2>
-            
-          </Box2>
+          <Box2></Box2>
           <Box2>
             <Box2_sub1>
               <Text> 요일별 추이 </Text>
@@ -428,6 +496,28 @@ function DustPage() {
             <Dustinfo2 />
           </Box3>
           <Box4>
+            <Text> 통합대기환경지수 </Text>
+            <Row>
+              <FaceImage src={face} />
+            </Row>
+            <Box4_sub1>
+              <Row>
+                <Text3> 아황산가스 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+              <Row>
+                <Text3> 일산화탄소 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+              <Row>
+                <Text3> 오존 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+              <Row>
+                <Text3> 이산화질소 </Text3>
+                <Text3> 000ppm </Text3>
+              </Row>
+            </Box4_sub1>
           </Box4>
         </Wrapper3>
       </Background>
