c37e4cf4d625bd0bab4e38b57f3565e69d994315
thals1214 <thaxls@ewhain.net>
2021-07-23 16:09:46 +0900

    Update user_action.js

--- src/_actions/user_action.js
+++ src/_actions/user_action.js
@@ -1,42 +1,61 @@
 import axios from 'axios'
-import {COVID_MAIN,COVID_NATIONAL,COVID_REGIONAL,LAT_TO_ADD} from './type'
+import {
+  COVID_MAIN,
+  COVID_NATIONAL,
+  COVID_REGIONAL,
+  LAT_TO_ADD,
+  DUST_MAIN,
+} from './type'
 
-import { USER_SERVER } from "../config";
+import { USER_SERVER } from '../config'
 
-
-
-export const covidMain =(lat, lon) =>{
-    const request=axios.get(`${USER_SERVER}/covid/main?lat=${lat}&lon=${lon}`)
+export const covidMain = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/covid/main?lat=${lat}&lon=${lon}`)
     .then(response => response.data)
-    return{
-        type:COVID_MAIN,
-        payload:request
-    }
+  return {
+    type: COVID_MAIN,
+    payload: request,
+  }
 }
 
-export const covidNational =() =>{
-    const request=axios.get(`${USER_SERVER}/covid/national`)
+export const covidNational = () => {
+  const request = axios
+    .get(`${USER_SERVER}/covid/national`)
     .then(response => response.data)
-    return{
-        type:COVID_NATIONAL,
-        payload:request
-    }
+  return {
+    type: COVID_NATIONAL,
+    payload: request,
+  }
 }
 
-export const covidRegional =(lat, lon) =>{
-    const request=axios.get(`${USER_SERVER}/covid/regional?lat=${lat}&lon=${lon}`)
+export const covidRegional = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/covid/regional?lat=${lat}&lon=${lon}`)
     .then(response => response.data)
-    return{
-        type:COVID_REGIONAL,
-        payload:request
-    }
+  return {
+    type: COVID_REGIONAL,
+    payload: request,
+  }
 }
 
-export const latToAdd =(lat, lon) =>{
-    const request=axios.get(`${USER_SERVER}/location/latLonToUmd?lat=${lat}&lon=${lon}`)
+export const latToAdd = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/location/latLonToUmd?lat=${lat}&lon=${lon}`)
     .then(response => response.data)
-    return{
-        type:LAT_TO_ADD,
-        payload:request
-    }
+  return {
+    type: LAT_TO_ADD,
+    payload: request,
+  }
+}
+
+// Micro Dust Page
+export const dustMain = (lat, lon) => {
+  const request = axios
+    .get(`${USER_SERVER}/microdust/main?lat=${lat}&lon=${lon}`)
+    .then(response => response.data)
+  return {
+    type: DUST_MAIN,
+    payload: request,
+  }
 }
