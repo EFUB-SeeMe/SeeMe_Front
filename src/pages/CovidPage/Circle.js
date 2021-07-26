d63ea15d2d38e8fb91b5657f8715d52a48144f43
정아연 <ribbon0529@naver.com>
2021-07-26 21:56:31 +0900

    add corona_num

--- src/pages/CovidPage/Circle.js
+++ src/pages/CovidPage/Circle.js
@@ -14,35 +14,54 @@ const Wrapper = styled.div`
   @media (min-width: 844px) and (max-width: 1440px) {
     //between
      margin-top:15px;
-     margin-right: 30px;
+     margin-right: 50px;
      width:150px;
+
   }
   @media (min-width: 420px) and (max-width: 843px) {
     // ipad
     margin-top:15px;
      margin-right: 0px;
      width:120px;
+
   }
 
 `
 
 const ColorCircle=styled.div`
-     position: relative;
-  display: inline-block;
-  margin: $spacing;
-  width: 120px;
-  height: 120px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
-  background-color: red;
- 
+  background-color: rgba(255,108,108,0.2);
+  border:8px solid rgba(255,108,108,1);
+  text-align: center;
+  font-size: 27px;
+  color:rgba(255,108,108,1);
+  font-family: 'NotoSansBold';
+  
 
 `
+
+const FontWrapper=styled.div`
+  font-size: 20px;
+  color:rgba(255,108,108,1);
+  font-family: 'NotoSansBold';
+  margin-top: 10px;
+  margin-left: 20px;
+`
+
+
 
 
 function Circle({text,num}) {
     return (
         <Wrapper >
-            <ColorCircle />
+           <ColorCircle > <p style={{fontSize:"15px",fontFamily:"NotoSans",margin:"0px"}}>안전지수</p> {97} </ColorCircle>
+           <FontWrapper>{"매우나쁨"}</FontWrapper>
         </Wrapper>
     )
 }
